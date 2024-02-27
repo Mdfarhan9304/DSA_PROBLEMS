dce370bfe9fb62eaa48aeaedba0034e9a8284901
Farhan Muzaffar <mdfarhan9304@gmail.com>
2024-02-27 14:48:00 +0530

    String compression problem

---
+++ String/Encode.java
@@ -0,0 +1,30 @@
+
+
+public class Encode {
+    
+    public static String encode(String str){
+        String newstr="";
+        for(int i=0; i<str.length(); i++){
+            Integer count=1;
+            while (i<str.length()-1 && str.charAt(i)== str.charAt(i+1)) {
+                count++;
+                i++;
+            }
+            newstr+=str.charAt(i);
+
+            if(count>1){
+                newstr+=count.toString();
+            }
+        }
+        return newstr;
+    }
+
+   
+
+    public static void main(String[] args) {
+        String words= "aaabbcddddde";
+        System.out.println(encode(words));
+
+
+    }
+}
