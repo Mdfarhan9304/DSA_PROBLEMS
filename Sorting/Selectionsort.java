1023ee511ca93a1843e46bce890fb9219d18744c
Farhan Muzaffar <mdfarhan9304@gmail.com>
2024-03-04 18:38:11 +0530

    Selection sorting  added

--- Sorting/Selectionsort.java
+++ Sorting/Selectionsort.java
@@ -1,19 +1,6 @@
 package Sorting;
-public class Bsort {
-    public static  void bsort(int arr[]){
-        for(int i=arr.length-1; i>=1;  i-- ){
-            for(int j=0; j<i; j++ ){
-                if(arr[j]> arr[j+1]){
-                    int temp= arr[j];
-                    arr[j]= arr[j+1];
-                    arr[j+1]= temp;
-                }
-             
-            }
-        }
-    
-    }
 
+public class Selectionsort {
     public static void ssort(int arr[]){
         int min, i, j;
         for( i=0; i<arr.length-1; i++){
@@ -29,13 +16,12 @@ public class Bsort {
 
         }
     }
-
+    
     public static void printArr(int arr[]){
         for (int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
     }
-  
     public static void main(String[] args) {
         int arr[]= {3, 6, 2, 9, 1};
         ssort(arr);
